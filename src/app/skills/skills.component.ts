8a9847ff9095afd398a86ddea7181e8b34bd192a
announa <moupou@posteo.org>
2022-03-02 18:10:05 -0500

    skills: shuffle animation

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -25,13 +25,15 @@ export class SkillsComponent implements OnInit {
     'Angular',
     'SCRUM',
     'Material',
-    ''
+    '',
   ];
+  skillPosition1: any[] = [];
 
   shuffle = 0;
 
   @ViewChildren('skillsHeading') skillsHeading!: QueryList<any>;
   @ViewChildren('skillContainer') skillContainer!: QueryList<any>;
+  @ViewChild('skillContainer') skillContainerEl!: ElementRef<any>;
   @ViewChildren('skill') domSkills!: QueryList<any>;
   skillArr!: ElementRef[];
 
@@ -45,13 +47,28 @@ export class SkillsComponent implements OnInit {
     this.observer.createIntersectionObserver(observeItems);
   }
 
-  shuffleSkills(){
-    this.skillArr.forEach(s => {
-      s.nativeElement.classList.remove('tt-0')
-      setTimeout(() => {
+  shuffleSkills() {
+    setTimeout(() => {
       this.shuffle++;
-      this.shuffle = this.shuffle % 3
-      s.nativeElement.classList.add('tt-0')
-    }, 500);})
+      this.shuffle = this.shuffle % 3;
+    }, 700);
+    for (let i = 0; i < this.skillArr.length; i++) {
+      this.skillArr[i].nativeElement.classList.remove('tt-0');
+      setTimeout(() => {
+        this.skillArr[i].nativeElement.classList.add('tt-0');
+      }, 700);
+    }
+  }
+
+  setShuffleClass(i: number) {
+    if (this.shuffle == 0) {
+      return 'skill' + i + '-0';
+    } else if (this.shuffle == 1) {
+      return 'skill' + i + '-1';
+    } else if (this.shuffle == 2){
+      return 'skill' + i + '-2';
+    } else {
+      return '';
+    }
   }
 }
