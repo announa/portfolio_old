0a6d895284a79abbcc47897474754fc24fb9445d
announa <moupou@posteo.org>
2022-03-04 17:59:10 -0500

    projects: texts and styles

--- src/app/projects.service.ts
+++ src/app/projects.service.ts
@@ -14,7 +14,7 @@ export class ProjectsService {
       link: 'bitcoin_app',
       description: 'A BTC/USD exchange calculator',
       description_long:
-        'This app calculates the currency exchange for the current exchange rate between Bitcoin and US Dollar. It also shows the historic exchange rate over a chosen period, represented in a table or a chart.',
+        'This app calculates the currency exchange for the current exchange rate between Bitcoin and US Dollar. It also shows the historic exchange rate over a chosen period, represented in a table or a chart. It was my first project implementing an API.',
       github: 'https://github.com/announa/bitcoin-app',
       url: 'https://annaludewig.net/projects/bitcoin-app/index.html',
       img: ['bitcoin.jpg', 'bitcoin_0.png', 'bitcoin_1.png', 'bitcoin_2.png'],
@@ -24,7 +24,7 @@ export class ProjectsService {
       title: 'El Pollo Loco',
       link: 'el_pollo_loco',
       description: 'A jump and run game',
-      description_long: 'This is a funny little game written in JavaScript.',
+      description_long: 'This is a funny little game written in JavaScript. It was my first project I realized using object orientated programming.',
       github: 'https://github.com/announa/el_pollo_loco',
       url: 'https://annaludewig.net/projects/el_pollo_loco/index.html',
       img: ['pollo_loco.jpg', 'pollo_loco_0.png', 'pollo_loco_1.png', 'pollo_loco_2.png'],
@@ -35,7 +35,7 @@ export class ProjectsService {
       link: 'pokedex',
       description: 'A pokemon gallery',
       description_long:
-        'In this Pokémon gallery you can find all the Pokémon or search them by name or their index. It uses the Pokeapi.',
+        'In this Pokémon gallery you can look up all the Pokémon or search them by name or their index. It uses the Pokeapi.',
       github: 'https://github.com/announa/pokedex',
       url: 'https://annaludewig.net/projects/pokedex/index.html',
       img: ['pokedex.jpg', 'pokedex_0.png', 'pokedex_1.png', 'pokedex_2.png'],
@@ -46,7 +46,7 @@ export class ProjectsService {
       link: 'ring_of_fire',
       description: 'A multiplayer online card game',
       description_long:
-        'A card game which can be played with multiple persons online. This game is built with TypeScript and Angular.',
+        'A card game which can be played with multiple persons online. The backend was realized using firebase. This game was my first bigger project I built with TypeScript and Angular.',
       github: 'https://github.com/announa/ring_of_fire',
       url: 'https://ring-of-fire-7eef3.web.app/',
       img: ['ring_of_fire.jpg', 'ring_of_fire_0.png', 'ring_of_fire_1.png', 'ring_of_fire_2.png'],
